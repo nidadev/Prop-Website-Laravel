b2e5e14c1fa107683c2bfdb818ccc83678a92a4f
nida <nidazeeshan15@gmail.com>
2024-08-01 19:54:03 +0500

    upd

---
+++ example-app/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+     //register api
+     public function register(Request $request)
+     {
+         
+     }
+     //login api
+     public function login(Request $request)
+     {
+         
+     }
+     //profile api
+     public function profile(Request $request)
+     {
+         
+     }
+     //logout api
+     public function logout(Request $request)
+     {
+         
+     }
+}
