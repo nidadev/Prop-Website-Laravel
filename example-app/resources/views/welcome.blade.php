f4b5639828f7ec5edb4f1f73d6f579ad8c99f20a
nida <nidazeeshan15@gmail.com>
2024-10-31 18:54:12 +0500

    upd

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -76,8 +76,8 @@ header .overlay {
 					 <!--h1><a class="text-white p-0 navbar-brand fw-bold logo_position_rel" href="{{ url('/home') }}"> PROPE <i class="fa fa-home col_blue me-1 logo_position_abs"></i> &nbsp;&nbsp;<span class="col_red">LYZE</span></a></h1-->
                      <p class="text-white mt-3 fs-5">  Empower Your Real Estate Campaigns with Precision and Speed</p>
 						<ul class="mb-0 mt-4">
-							<li class="d-inline-block"><a class="button" href="<a class="text-white" href="{{ url('/register')}}">"><i class="fa fa-home me-1 align-middle fs-5"></i> Land </a></li>
-							<li class="d-inline-block ms-2"><a class="button_1" href="<a class="text-white" href="{{ url('/register')}}">"><i class="fa fa-building me-1 fs-5 align-middle"></i> Houses </a></li>
+							<li class="d-inline-block"><a class="button" href="{{ url('/register')}}"><i class="fa fa-home me-1 align-middle fs-5"></i> Land </a></li>
+							<li class="d-inline-block ms-2"><a class="button_1" href="{{ url('/register')}}"><i class="fa fa-building me-1 fs-5 align-middle"></i> Houses </a></li>
 						</ul>
       </div>
     </div>
